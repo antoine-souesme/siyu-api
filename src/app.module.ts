be76a94e046cc51ef3e072680bf2f8df2e4fae53
Antoine Souesme <antoine@lapala.io>
2025-03-16 10:17:57 +0100

    Added typeorm and connection to postgress database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-    imports: [],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: 'localhost',
+            port: 5455,
+            username: 'root',
+            password: 'root',
+            database: 'siyu',
+            entities: [],
+            migrations: [],
+
+            // FIXME: Remove in production
+            synchronize: true,
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
