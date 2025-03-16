ed0c515f88a3a05c7bf5b698338324d8830da5a4
Antoine Souesme <antoine@lapala.io>
2025-03-16 12:33:00 +0100

    Setup swagger and open-api

--- src/features/users/user.entity.ts
+++ src/features/users/user.entity.ts
@@ -1,7 +1,11 @@
 import { BaseEntity } from "@/utils/base.entity";
-import { Entity } from "typeorm";
+import { Column, Entity } from "typeorm";
 
 @Entity()
 export class User extends BaseEntity {
+    @Column()
+    email: string;
 
+    @Column()
+    displayName: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './features/app/app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    //>────────────────────────────────────────────────────────────────────────────────────────────────<
+    //> Swagger                                                                                        <
+    //>────────────────────────────────────────────────────────────────────────────────────────────────<
+
+    const config = new DocumentBuilder()
+        .setTitle('Documentation')
+        .setDescription('Siyu API documentation')
+        .setVersion('0.1')
+        .build();
+
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup('api', app, documentFactory);
+
+    //>────────────────────────────────────────────────────────────────────────────────────────────────<
+    //> Launch application                                                                             <
+    //>────────────────────────────────────────────────────────────────────────────────────────────────<
+
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
