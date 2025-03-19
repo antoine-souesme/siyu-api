6287fdfd011dd52fa3e25236f10e1e4cd3ca58bc
Antoine Souesme <antoine@lapala.io>
2025-03-19 11:56:22 +0100

    Decorators for error codes

--- src/features/auth/auth.controller.ts
+++ src/features/auth/auth.controller.ts
@@ -3,7 +3,7 @@ import { LoginDto } from '@/features/auth/dtos/login.dto';
 import { RegisterDto } from '@/features/auth/dtos/register.dto';
 import { LoginResponse } from '@/features/auth/responses/login.response';
 import { RegisterResponse } from '@/features/auth/responses/register.response';
-import { ErrorResponse } from '@/utils/decorators';
+import { ErrorResponse } from '@/utils/errors';
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApiOkResponse } from '@nestjs/swagger';
 
